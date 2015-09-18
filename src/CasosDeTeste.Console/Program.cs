46338b3636c1b23c06afccda5d2aa453813b027d
David Rodrigues <david.rodrigues@inventsoftware.com.br>
2015-09-18 07:52:05 -0300

    Estrutura inicial do projeto

---
+++ src/CasosDeTeste.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CasosDeTeste.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/CasosDeTeste.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CasosDeTeste.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal("1", "1");
+        }
+    }
+}
